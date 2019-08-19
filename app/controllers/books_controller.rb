1713e1c4bf2209c7f45b1921f5ab8e6653dffbc6
suzutatsu <suzuta1221@gmail.com>
2019-08-19 15:27:30 +0900

    [update]5-9

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,9 +1,16 @@
 class BooksController < ApplicationController
 
+def top
+end
+
 def new
+	@book = Book.new
 end
 
 def create
+	book = Book.new(book_params)
+	book.save
+	redirect_to book_path
 end
 
 def index
@@ -15,7 +22,10 @@ end
 def edit
 end
 
-
+private
+def book_params
+	params.require(:book).permit(:title, :body)
+end
 
 
 
